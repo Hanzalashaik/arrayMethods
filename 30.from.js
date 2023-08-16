4c45773a3bdb21effeacbdd2772b996d4cdfa13d
Hanzalashaik <shaikhhanzala27@gmail.com>
2023-08-16 16:45:01 +0000

    From Method

---
+++ 30.from.js
@@ -0,0 +1,13 @@
+const newarr=Array.from("abc");
+console.log(newarr);
+
+//Creating Array from string 
+const arr2=Array.from("javaScript")
+console.log(arr2);
+
+
+//creating set from array
+let set=new Set(["javascript","python","GO","python"])
+
+let newarray=Array.from(set)
+console.log(newarray);
